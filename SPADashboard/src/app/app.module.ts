db013ea38e41fd10ecde92dd546170c7e12c426e
Rodrigo Rabelo <rodrigoalexandrerabelo@gmail.com>
2019-12-28 16:17:47 -0300

    Add sidenav, navbar component

--- SPADashboard/src/app/app.module.ts
+++ SPADashboard/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule
