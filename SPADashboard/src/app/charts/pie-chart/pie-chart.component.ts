265d8dbcd9d4f6c907c7437a1be4047dafe8b56c
Rodrigo Rabelo <rodrigoalexandrerabelo@gmail.com>
2020-01-05 22:41:56 -0300

    Add pie chart component data

--- SPADashboard/src/app/charts/pie-chart/pie-chart.component.ts
+++ SPADashboard/src/app/charts/pie-chart/pie-chart.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+  public pieChartData: number[] = [350, 450, 120];
+  public pieChartLabel: string[] = ['XYZ Logistic', 'Main St Bakery', 'Acme Hosting'];
+  public pieChartType = 'doughnut';
+  public pieChartLegend = true;
+  public colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
 
   ngOnInit() {
   }
